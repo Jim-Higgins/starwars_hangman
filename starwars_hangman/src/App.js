bb4cb534d432a4234f76ccc4c8f357bb50aa80ac
Jim Higgins <Jim.Higgins@onthebeach.co.uk>
2019-10-10 12:49:10 +0100

    usedletter button working progress

--- starwars_hangman/src/App.js
+++ starwars_hangman/src/App.js
@@ -2,16 +2,38 @@ import React from 'react';
 
 import './css/App.css';
 import Header from "./Components/Header"
+import LetterButton from './Components/LetterButton';
 
 class App extends React.Component {
+
+  state = {
+    availableLetters: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+    pickedChosenLetters: '',
+    generatedWord: 'Wars',
+    usedLetters: ''
+  }
 
   render(){
     return (
       <div className="App">
         <Header/>
+
+
+        {this.state.availableLetters.map((letter, index) =>{
+         return <LetterButton key={index} letter={letter} onClick={this.handleClick}/>
+        })}
       </div>
     );
   }
+
+  handleClick = (e) =>{
+    const value = e.target.value
+    e.target.disabled = true
+    console.log(e.target)
+    this.setState(prevState => ( {
+      usedLetters: prevState.usedLetters + value
+    })
+  )}
 }
 
 export default App;
---
+++ starwars_hangman/src/Components/LetterButton.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LetterButton = ({letter, onClick}) => {
+    return <button onClick={onClick} value={letter} disabled={false} >{letter}</button>
+};
+
+export default LetterButton;
